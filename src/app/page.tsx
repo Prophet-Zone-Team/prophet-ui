51d1a3900791698f8cf5daaa4c9108d2ee9fc1e6
jimmygu <jimmygujh@gmail.com>
2026-05-27 17:36:28 +0800

    Implement private mode routing: Redirect users to the private top-up page based on hostname checks in RootPage and FifaWinnerPage. Add new private page component for top-up functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { isPrivateModeHost } from "@/config/funding";
-import { PrivateTopupPage } from "@/views/portfolio/private-topup";
 
 export default async function RootPage() {
   const hostHeader = (await headers()).get("host") ?? "";
   const hostname = hostHeader.split(":")[0] ?? "";
 
   if (isPrivateModeHost(hostname)) {
-    return <PrivateTopupPage />;
+    redirect("/private");
   }
 
   redirect("/fifa");
