f0a5a2e0efada0db89ad04eb604252226c5b9d38
Luna <luna.spark9234@proton.me>
2026-06-01 16:03:14 +0800

    fix cursor.

--- src/views/trade/shared/game-outcome-bid-buttons.tsx
+++ src/views/trade/shared/game-outcome-bid-buttons.tsx
@@ -62,7 +62,7 @@ export function GameOutcomeBidButton({
       className={cn(
         "flex w-full items-center justify-center border-0 font-[556] text-white transition-opacity",
         sizeClass.button,
-        isInteractive ? "cursor-pointer" : "cursor-not-allowed",
+        isInteractive ? "cursor-pointer" : "",
         disabled
           ? "opacity-40"
           : isInteractive
