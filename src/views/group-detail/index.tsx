036f83f477b8a3b57a804677592e3597d9392e6e
Luna <luna.spark9234@proton.me>
2026-06-12 14:39:44 +0800

    file

---
+++ src/views/group-detail/index.tsx
@@ -0,0 +1,13 @@
+import { PageBack } from "@/components/ui/page-back";
+
+export default function GroupDetail() {
+  return (
+    <div className="mx-auto">
+      <PageBack />
+      <div className="flex justify-center gap-2">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
