e12fff152dea537817dbfb44e38a05b1cfedcc65
Luna <luna.spark9234@proton.me>
2026-06-13 18:19:12 +0800

    add dynamic.

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,4 +1,11 @@
-import { PortfolioView } from "@/views/portfolio";
+import dynamic from "next/dynamic";
+
+import PortfolioLoading from "./loading";
+
+const PortfolioView = dynamic(
+  () => import("@/views/portfolio").then((mod) => mod.PortfolioView),
+  { loading: () => <PortfolioLoading /> },
+);
 
 export default function Page() {
   return <PortfolioView />;
