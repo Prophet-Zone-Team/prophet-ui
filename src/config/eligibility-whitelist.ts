64e60133f0c82bbf65b067a8a84f00138fffbf47
Luna <luna.spark9234@proton.me>
2026-06-24 15:15:21 +0800

    whitelist.

--- src/config/eligibility-whitelist.ts
+++ src/config/eligibility-whitelist.ts
@@ -1,5 +1,6 @@
 /** Raw whitelist entries; normalized at runtime by server helpers. */
 export const ELIGIBILITY_WHITELIST_EMAILS: string[] = [
   "emily@ref.finance",
-  "amy@ref.finance"
+  "amy@ref.finance",
+  "w583003181@gmail.com"
 ];
